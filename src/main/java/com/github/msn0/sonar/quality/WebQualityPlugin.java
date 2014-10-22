a3ef508390422d8394f1aba8891b195653a9d2a7
Michał Jezierski <ambinanitelo@gmail.com>
2014-10-22 05:38:41 +0200

    remove unused WidgetFooter

--- src/main/java/com/github/msn0/sonar/quality/WebQualityPlugin.java
+++ src/main/java/com/github/msn0/sonar/quality/WebQualityPlugin.java
@@ -2,7 +2,6 @@ package com.github.msn0.sonar.quality;
 
 import com.github.msn0.sonar.quality.batch.PsiSensor;
 import com.github.msn0.sonar.quality.ui.RubyWidget;
-import com.github.msn0.sonar.quality.ui.WidgetFooter;
 import org.sonar.api.Properties;
 import org.sonar.api.Property;
 import org.sonar.api.SonarPlugin;
@@ -24,7 +23,6 @@ public final class WebQualityPlugin extends SonarPlugin {
         return Arrays.asList(
                 WebQualityMetrics.class,
                 PsiSensor.class,
-                WidgetFooter.class,
                 RubyWidget.class
         );
     }
